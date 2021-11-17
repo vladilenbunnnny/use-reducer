eaedb33dbe5fbe920cc209c715736dabfbb291b2
thebunnnny <thebunnnny@gmail.com>
2021-11-17 12:14:00 +1000

    classic state count implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useReducer } from "react";
+import "./App.css";
 
 function App() {
+  // Reducer state //
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "increment":
+        return { count };
+    }
+  };
+  const initialValue = {
+    count: 0,
+  };
+  const [state, dispatch] = useReducer(reducer, initialValue);
+  // Reducer state //
+
+  // Classic state //
+  const [count, setCount] = useState(0);
+
+  const like = () => {
+    setCount(prev => prev + 1);
+  };
+  const disLike = () => {
+    if (count > 0) {
+      setCount(prev => prev - 1);
+    } else {
+      return count;
+    }
+  };
+  // Classic state //
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Classic state</h1>
+        <p>Count is: {count}</p>
+        <button onClick={like}>Like</button>
+        <button onClick={disLike}>Dislike</button>
+      </div>
+      <div>
+        <h1>Reducer state</h1>
+        <p>Count is: {state.count}</p>
+        <button>Like</button>
+        <button>Dislike</button>
+      </div>
     </div>
   );
 }
